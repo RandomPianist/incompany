7e72843fd06cda10a92ea604d9946ff295aad12d
RandomPianist <reynoldscosta@hotmail.com>
2025-07-25 14:42:40 -0300

    Correção lexical

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
         <meta charset = "utf-8" />
         <meta name = "csrf-token" content = "{{ csrf_token() }}" />
         <meta name = "viewport" content = "width=device-width, initial-scale=1" />
-        <title>Kx-safe</title>
+        <title>Loja Incompany</title>
         <link rel = "stylesheet" href = "https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" />
         <link rel = "stylesheet" href = "{{ asset('css/app.css') }}" />
         <link rel = "stylesheet" href = "{{ asset('css/my-style.css') }}" />
