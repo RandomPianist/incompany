cedf79f3798863003099d7197cf1c859ae232df0
RandomPianist <reynoldscosta@hotmail.com>
2025-08-02 12:21:00 -0300

    Impedir criação de produto

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -54,9 +54,6 @@
             </div>
         </div>
     </div>
-    <button class = "btn btn-primary custom-fab" type = "button" onclick = "chamar_modal(0)">
-        <i class = "my-icon fas fa-plus"></i>
-    </button>
     <script type = "text/javascript" language = "JavaScript">
         let ant_consumo = false;
 
