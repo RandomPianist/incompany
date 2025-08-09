e5f1260bf5e83eb2b53610fd2ffc6b60825c401a
RandomPianist <reynoldscosta@hotmail.com>
2025-08-09 09:00:45 -0300

    Processo de arquivo morto

---
+++ arquivo_morto.sql
@@ -0,0 +1,37 @@
+CREATE TABLE logbkp AS SELECT * FROM log;
+CREATE TABLE log2 AS (
+	SELECT log.*
+	
+	FROM log
+	
+	JOIN (
+		SELECT
+			MAX(data) AS data,
+			MAX(created_at) AS created_at,
+			tabela,
+			acao
+			
+		FROM log
+		
+		WHERE tabela NOT IN ('retiradas', 'estoque', 'solicitacoes')
+		
+		GROUP BY
+			tabela,
+			acao
+	) AS lim ON lim.data = log.data
+		AND lim.created_at = log.created_at
+		AND lim.tabela = log.tabela
+		AND lim.tabela = log.acao
+		
+	UNION ALL (
+		SELECT *
+		
+		FROM log
+		
+		WHERE tabela IN ('retiradas', 'estoque', 'solicitacoes')
+	)
+);
+DROP TABLE log;
+RENAME TABLE log2 TO log;
+ALTER TABLE log ADD PRIMARY KEY (id);
+ALTER TABLE log MODIFY id int(11) NOT NULL AUTO_INCREMENT;
