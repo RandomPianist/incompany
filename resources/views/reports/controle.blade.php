2f308cb57cd51e71bdf5ebeb958c7f05c23cbe17
RandomPianist <reynoldscosta@hotmail.com>
2025-09-11 08:12:17 -0300

    Identação do termo de retirada

--- resources/views/reports/controle.blade.php
+++ resources/views/reports/controle.blade.php
@@ -2,20 +2,19 @@
 
 @section("content")
     @foreach ($resultado AS $item)
-    <div class = "report-header">
-        <div class = "float-left">
-            <div>
-                <span>{{ $resultado[0]["empresa"] }}</span>
-            </div>
-            <div>
-                <span>CNPJ: {{ $resultado[0]["cnpj"] }}</span>
+        <div class = "report-header">
+            <div class = "float-left">
+                <div>
+                    <span>{{ $resultado[0]["empresa"] }}</span>
+                </div>
+                <div>
+                    <span>CNPJ: {{ $resultado[0]["cnpj"] }}</span>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="nome-rel">
-        <span class="m-auto">Controle de Entrega e Reposição de Equipamentos de Proteção Individual - E.P.I.</span>
-    </div>
-    
+        <div class = "nome-rel">
+            <span class = "m-auto">Controle de Entrega e Reposição de Equipamentos de Proteção Individual - E.P.I.</span>
+        </div>
         <div class = "d-grid">
             <div class = "c-1">Nome do Funcionário: {{ $item["nome"] }}</div>
             <div class = "c-2">CPF: {{ $item["cpf"] }}</div>
@@ -71,39 +70,42 @@
             </table>
         </div>
         <div class = "line-div"></div>
-    
-    <div class="rep-ret-rodape">
-        <p>Declaro para os devidos fins, que recebi gratuitamente os E.P.I.s e/ou uniforme acima descritos e me comprometo:
-        <ul>
-            <li>Usá-los apenas para a finalidade a que se destinam;</li>
-            <li>Responsabilizar-me por sua guarda e conservação;</li>
-            <li>Comunicar ao empregador qualquer modificação que os tornem imprópios para o uso;</li>
-            <li>Responsabilizar-me pela danificação do E.P.I e/ou uniforme devido ao uso inadequado ou fora das atividades a que se destina, bem como pelo seu extravio;</li>
-            <li>Ciente que serei advertido de acordo com o artigo 482 da CLT se não fizer uso devido dos E.P.I e uniformes entregues a mim.</li>
-        </ul>
-        <p>Declaro estar ciente de que o uso <span class="bold">é obrigatório</span> enquanto eu estiver exercendo minhas funções.</p>
-        <p>Sob pena de ser punido conforme Lei n. 6.514, de 22/12/77, artigo 158</p>
-        <p>Declaro, ainda que recebi treinamento e orientação referente ao uso do E.P.I e as Normas de Segurança do Trabalho.</p>
-        <div class="data-extenso">
-            <span class="traduzir">{{$cidade}}, {{$data_extenso}}</span>
+        <div class = "rep-ret-rodape">
+            <p>Declaro para os devidos fins, que recebi gratuitamente os E.P.I.s e/ou uniforme acima descritos e me comprometo:
+            <ul>
+                <li>Usá-los apenas para a finalidade a que se destinam;</li>
+                <li>Responsabilizar-me por sua guarda e conservação;</li>
+                <li>Comunicar ao empregador qualquer modificação que os tornem imprópios para o uso;</li>
+                <li>Responsabilizar-me pela danificação do E.P.I e/ou uniforme devido ao uso inadequado ou fora das atividades a que se destina, bem como pelo seu extravio;</li>
+                <li>Ciente que serei advertido de acordo com o artigo 482 da CLT se não fizer uso devido dos E.P.I e uniformes entregues a mim.</li>
+            </ul>
+            <p>Declaro estar ciente de que o uso <span class = "bold">é obrigatório</span> enquanto eu estiver exercendo minhas funções.</p>
+            <p>Sob pena de ser punido conforme Lei n. 6.514, de 22/12/77, artigo 158</p>
+            <p>Declaro, ainda que recebi treinamento e orientação referente ao uso do E.P.I e as Normas de Segurança do Trabalho.</p>
+            <div class = "data-extenso">
+                <span class = "traduzir">{{$cidade}}, {{$data_extenso}}</span>
+            </div>
+            <div class = "assinatura">
+                <div class = "asn_1">
+                    <span>{{$resultado[0]["empresa"]}}</span>
+                </div>
+                <div class = "asn_2">
+                    <span>{{$item["nome"]}}</span>
+                </div>
+            </div>
+            <div class = "assinatura-print" style = "margin-top:80px">
+                <table class = "table" style = "border-style:border-none">
+                    <tr>
+                        <td class = "text-center" width = "50%" style = "vertical-align:bottom">________________________________________________</td>
+                        <td class = "text-center" width = "50%" style = "vertical-align:bottom">________________________________________________</td>
+                    </tr>
+                    <tr>
+                        <td class = "text-center" width = "50%" style = "vertical-align:top">{{$resultado[0]["empresa"]}}</td>
+                        <td class = "text-center" width = "50%" style = "vertical-align:top">{{$item["nome"]}}</td>
+                    </tr>
+                </table>
+            </div>
         </div>
-        <div class="assinatura">
-            <div class="asn_1"><span>{{$resultado[0]["empresa"]}}</span></div>
-            <div class="asn_2"><span> {{$item["nome"]}}</span></div>
-        </div>
-        <div class="assinatura-print" style = "margin-top:80px">
-            <table class = "table" style = "border-style:border-none">
-                <tr>
-                    <td class = "text-center" width = "50%" style = "vertical-align:bottom">________________________________________________</td>
-                    <td class = "text-center" width = "50%" style = "vertical-align:bottom">________________________________________________</td>
-                </tr>
-                <tr>
-                    <td class = "text-center" width = "50%" style = "vertical-align:top">{{$resultado[0]["empresa"]}}</td>
-                    <td class = "text-center" width = "50%" style = "vertical-align:top">{{$item["nome"]}}</td>
-                </tr>
-            </table>
-        </div>
-    </div>
-    <div class = "pagebreak2"></div>
+        <div class = "pagebreak2"></div>
     @endforeach
 @endsection
