50b389b4878e91cacd6dc269769f4538608093f7
RandomPianist <reynoldscosta@hotmail.com>
2025-10-06 11:50:12 -0300

    Correção

--- resources/views/modals/setores_modal.blade.php
+++ resources/views/modals/setores_modal.blade.php
@@ -56,7 +56,7 @@
                         </div>
                     </div>
                 </div>
-                <div class = "d-flex">
+                <div class = "d-flex mt-4">
                     <button type = "button" class = "btn btn-target mx-auto mb-4 px-5" onclick = "validar()">Salvar</button>
                 </div>
             </form>
