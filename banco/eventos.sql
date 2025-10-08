c56c9a0be9f43f124765a06f8bf8b835f1261ad7
RandomPianist <reynoldscosta@hotmail.com>
2025-10-08 12:23:48 -0300

    Correção

--- banco/eventos.sql
+++ banco/eventos.sql
@@ -7,10 +7,10 @@ STARTS TIMESTAMP(CURRENT_DATE, '02:00:00')
 DO
 BEGIN
     TRUNCATE TABLE mat_vultretirada;
+    CALL excluir_atribuicao_sem_retirada();
     CALL refazer_ids();
     CALL reindexar();
     CALL limpar_usuario_editando();
-    CALL excluir_atribuicao_sem_retirada();
     CALL atualizar_mat_vcomodatos(0);
     CALL atualizar_mat_vatbaux('T', '(0)', 'S');
     CALL atualizar_mat_vatribuicoes('T', '(0)', 'S');
