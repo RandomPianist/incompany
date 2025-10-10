bd88e52f7a03ee1e11ad4f104fecbd1c67efab12
RandomPianist <reynoldscosta@hotmail.com>
2025-10-10 16:27:37 -0300

    Correção

--- resources/views/mensagem.blade.php
+++ resources/views/mensagem.blade.php
@@ -4,8 +4,8 @@
         <meta charset = "utf-8" />
         <meta name = "viewport" content = "width=device-width, initial-scale=1" />
         <title>Loja Incompany</title>
-        <link rel = "icon shortcut" href = "{{ asset('img/favicon.ico') }}" type = "image/x-icon" />
-        <link rel = "icon"          href = "{{ asset('img/favicon.ico') }}" type = "image/x-icon" />
+        <link rel = "icon shortcut" href = "{{ asset('storage/favicon.ico') }}" type = "image/x-icon" />
+        <link rel = "icon"          href = "{{ asset('storage/favicon.ico') }}" type = "image/x-icon" />
         <link href = "{{ asset('css/geral/app.css') }}" rel = "stylesheet" />
     </head>
     <body>
